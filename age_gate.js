ad04a5075a76ec488e1e910a48bb4216e04eeea7
Nick <ntomsho4545@gmail.com>
2019-09-08 15:28:55 -0400

    Implement headline with dynamic minimum age

--- age_gate.js
+++ age_gate.js
@@ -1,11 +1,27 @@
-// Implement as document ready callback
+// Implement age gate code as document ready callback
 $( document ).ready(function() {
 
+    //Create headline with minimum age
+    $( "h3" ).html(`You must be at least ${MINAGE} to access this site`);
+
+    //Add months to month selector
+    $( "#month-select" ).append(MONTHS.map(month => {
+        return `<option value=${month}>${month}</option>`
+    }));
+
+    //Add years to year selector
+    // $( "year-select" ).append()
+    
+    //Calculate age as an integer from date of birth
     function calculateAge(dob) {
         const dobDate = new Date(dob);
         const now = Date.now();
         const age = Date(now - dobDate)
         return Math.abs(age.getUTCFullYear() - 1970);
     }
-    
-})
+
+});
+
+// Constants
+var MINAGE = 21;
+var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
