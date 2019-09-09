6d63688e3c3f31208454b62818387928c5d6025c
Nick <ntomsho4545@gmail.com>
2019-09-09 00:36:49 -0400

    Implement first pass of remember me cookie function

--- age_gate.js
+++ age_gate.js
@@ -1,4 +1,3 @@
-// Implement age gate code as document ready callback
 $( document ).ready(function() {
 
     //Create headline with minimum age
@@ -12,30 +11,49 @@ $( document ).ready(function() {
         return `<option value=${month}>${month}</option>`
     }));
 
-    //Add event listener to verify age button
+    //Add event listeners
     $( "#verify-button" ).click(verifyAge);
+    $( "#cookie-check" ).click(cookieCheck);
 
-    //Begin age verification logic
-    function verifyAge() {
-        //** */TBD day selector
-        let dob = $( "#month-select" ).val() + " 1, " + $( "#year-select" ).val();
-        //** */Need error handling here for incomplete entry
-        if (calculateAge(dob) >= MINAGE) {
-            console.log("Welcome");
-        } else {
-            console.log("GTFO");
+    //Cookie functions
+        //Set cookie value
+        function setCookie(value) {
+            debugger
+            document.cookie = `rememberme=${value}; path=/`;
         }
-    }
+
+        //Handle remember me checkbox
+        function cookieCheck() {
+            if ($( "#cookie-check" ).is(":checked")) { 
+                setCookie("true");
+                console.log(document.cookie);
+            } else {
+                setCookie("");
+                console.log(document.cookie);
+            }
+        }
     
-    //Calculate age as an integer from date of birth
-    function calculateAge(dob) {
-        debugger
-        const age = new Date(Date.now() - new Date(dob))
-        debugger
-        return Math.abs(age.getUTCFullYear() - 1970);
-    }
+    //Age verification functions
+        //Begin age verification logic
+        function verifyAge() {
+            //** */TBD day selector
+            let dob = $("#month-select").val() + " 1, " + $("#year-select").val();
+            //** */Need error handling here for incomplete entry
+            if (calculateAge(dob) >= MINAGE) {
+                console.log("Welcome");
+            } else {
+                console.log("GTFO");
+            }
+        }
+
+        //Calculate age as an integer from date of birth
+        function calculateAge(dob) {
+            const age = new Date(Date.now() - new Date(dob))
+            return Math.abs(age.getUTCFullYear() - 1970);
+        }
 
 });
+
 
 // Constants
 var MINAGE = 21;
