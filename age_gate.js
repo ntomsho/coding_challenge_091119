f4bf91d8784dea184675fdd3808dfd5f2ec8aeb5
Nick <ntomsho4545@gmail.com>
2019-09-10 22:31:38 -0400

    Implement filters to ensure dates are valid

--- age_gate.js
+++ age_gate.js
@@ -1,25 +1,58 @@
 $( document ).ready(function() {
 
     //Create headline with minimum age
-    $( "h3" ).html(`You must be at least <strong>${MINAGE}</strong> to access this site`);
+    $( "h3" ).html(`You must be at least <br/><strong>${MINAGE}</strong><br/> to access this site`);
 
     //Create button text with minimum age
     $( "#underage-button" ).html(`I'm not ${MINAGE}`);
 
     //Add event listeners
     $( "#verify-button" ).click(verifyAge);
-    // $( "#cookie-check" ).click(cookieCheck);
+    $( ":text" ).keypress(isNumber);
+    $( ":text" ).keyup(isValid);
 
-    //Check for cookie, if present, set all values to cookie values
-    const cookieCheck = checkForCookie();
-    if (cookieCheck) {
-        $( "#month-select" ).val(cookieCheck["month"]);
-        $( "#day-select" ).val(cookieCheck["day"]);
-        $( "#year-select" ).val(cookieCheck["year"]);
-        $( "#cookie-check" ).prop("checked", true);
-    };
+    //Manage inputs to ensure valid dates
+    function isNumber(event) {
+        //Confirm that key pressed is a number
+        const kc = event.keyCode;
+        if (!(kc >= 48 && kc <= 57)) {
+            return false;
+        }
+    }
 
+    function isValid(event) {
+        //Confirm that input is in correct range
+        const value = parseInt(event.currentTarget.value);
+        let range = [];
+        let len = 2;
+        switch (event.currentTarget.id) {
+            case "month-select":
+                range = [1, 12];
+                break;
+            case "day-select":
+                range = [1, MONTHDAYS[$("#month-select").val()] || 31];
+                break;
+            case "year-select":
+                const now = new Date;
+                range = [1900, now.getFullYear()];
+                len = 4;
+        }
+        //If entered value is sufficient length, check if it is within the correct range
+        if (value.toString().length >= len && (value < range[0] || value > range[1])) {
+            $(event.currentTarget).val("");
+        }
+    }
+    
     //Cookie functions
+        //Check for cookie, if present, set all values to cookie values
+        const cookieCheck = checkForCookie();
+        if (cookieCheck) {
+            $("#month-select").val(cookieCheck["month"]);
+            $("#day-select").val(cookieCheck["day"]);
+            $("#year-select").val(cookieCheck["year"]);
+            $("#cookie-check").prop("checked", true);
+        };
+
         //Check for cookie
         function checkForCookie() {
             //Break cookie string down into array of key-value pairs
@@ -77,4 +110,5 @@ $( document ).ready(function() {
 
 // Constants
 var MINAGE = 21;
-var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var MONTHDAYS = {"1":31, "2":29, "3":31, "4":30, "5":31, "6":30, "7":31, "8":31, "9":30, "10":31, "11":30, "12":31,
+"01":31, "02":29, "03":31, "04":30, "05":31, "06":30, "07":31, "08":31, "09":30}
